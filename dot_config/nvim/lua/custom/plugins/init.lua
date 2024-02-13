083cfe453946a1efc5e2be29986b86a4f8fdcc83
Basix <basix@basix.tech>
2024-02-13 22:25:58 +0900

    Add Copilot

--- dot_config/nvim/lua/custom/plugins/init.lua
+++ dot_config/nvim/lua/custom/plugins/init.lua
@@ -2,4 +2,15 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  { 'lbrayner/vim-rzip', lazy = true },
+  {
+    'zbirenbaum/copilot.lua',
+    lazy = true,
+    cmd = 'Copilot',
+    event = 'InsertEnter',
+    config = function()
+      require('copilot').setup()
+    end,
+  }
+}
