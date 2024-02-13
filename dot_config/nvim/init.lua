4702eeea9d85f773f9ae0058db8f7b2c7376b986
Basix <basix@basix.tech>
2024-02-13 22:26:11 +0900

    Enable rust-analyzer and tsserver

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -546,8 +546,8 @@ local servers = {
   -- clangd = {},
   -- gopls = {},
   -- pyright = {},
-  -- rust_analyzer = {},
-  -- tsserver = {},
+  rust_analyzer = {},
+  tsserver = {},
   -- html = { filetypes = { 'html', 'twig', 'hbs'} },
 
   lua_ls = {
