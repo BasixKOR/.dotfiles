94a10e5b8c117503d7c7862844b19c5a2aa7646f
Basix <basix@basix.tech>
2024-02-14 18:17:35 +0900

    Use non-daemon variant of JS tools
    Turns out it's only one with proper Yarn PnP support.

--- dot_config/nvim/lua/config/none-ls.lua
+++ dot_config/nvim/lua/config/none-ls.lua
@@ -7,9 +7,9 @@ return {
 		-- https://github.com/nvimtools/none-ls.nvim/blob/main/doc/BUILTINS.md#ruff-1
 		null_ls.setup({
 			sources = {
-				null_ls.builtins.diagnostics.eslint_d,
+				null_ls.builtins.diagnostics.eslint,
 				null_ls.builtins.code_actions.shellcheck,
-				null_ls.builtins.formatting.prettierd,
+				null_ls.builtins.formatting.prettier,
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.ruff_format,
 				null_ls.builtins.formatting.biome,
