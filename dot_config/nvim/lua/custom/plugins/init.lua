4fe3f1fe51fef5f7701f9f879c2fd972246b1092
Basix <basix@basix.tech>
2024-03-05 17:58:34 +0900

    Enable oil keymap

--- dot_config/nvim/lua/custom/plugins/init.lua
+++ dot_config/nvim/lua/custom/plugins/init.lua
@@ -27,6 +27,9 @@ return {
   {
     'stevearc/oil.nvim',
     opts = {},
+    config = function ()
+      vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+    end
     -- Optional dependencies
     -- dependencies = { "nvim-tree/nvim-web-devicons" },
   },
