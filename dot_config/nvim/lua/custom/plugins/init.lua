2e50338e2d3a1551add0f6d8f69cfa3ff91f1873
Basix <basix@basix.tech>
2024-03-08 06:23:13 +0900

    Add Gitsigns

--- dot_config/nvim/lua/custom/plugins/init.lua
+++ dot_config/nvim/lua/custom/plugins/init.lua
@@ -37,5 +37,9 @@ return {
     "pmizio/typescript-tools.nvim",
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
     opts = {},
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {},
   }
 }
