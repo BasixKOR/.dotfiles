db8d44351b17262982d4ae5dd44772395d0efd62
Basix <basix@basix.tech>
2024-04-27 09:29:58 +0900

    Configure YAML Language Server for Neovim

--- dot_config/nvim/lua/custom/lsp.lua
+++ dot_config/nvim/lua/custom/lsp.lua
@@ -89,6 +89,26 @@ local servers = {
       -- diagnostics = { disable = { 'missing-fields' } },
     },
   },
+
+  yamlls = {
+    schemas = {
+      kubernetes = "*.yaml",
+      ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+      ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+      ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
+      ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+      ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+      ["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
+      ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+      ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
+      ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
+      ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
+      ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] =
+      "*docker-compose*.{yml,yaml}",
+      ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] =
+      "*flow*.{yml,yaml}",
+    }, -- https://www.reddit.com/r/neovim/comments/ze9gbe/comment/iz59clw/
+  }
 }
 
 -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
