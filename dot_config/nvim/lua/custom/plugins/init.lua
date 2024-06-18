70f16b3bf07f3320fbf65e6f6150ffdeecc08a25
Basix <basix@basix.tech>
2024-06-18 21:20:01 +0900

    Remove nvim-auto-session

--- dot_config/nvim/lua/custom/plugins/init.lua
+++ dot_config/nvim/lua/custom/plugins/init.lua
@@ -61,13 +61,6 @@ return {
     end,
   },
   {
-    'rmagatti/auto-session',
-    opts = {
-      log_level = 'error',
-      auto_session_suppress_dirs = { '~/', '~/work', '~/Downloads', '/', '/tmp' },
-    },
-  },
-  {
     'kylechui/nvim-surround',
     version = '*', -- Use for stability; omit to use `main` branch for the latest features
     event = 'VeryLazy',
