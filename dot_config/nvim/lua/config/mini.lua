29058aa2618a9ac44ec758ca4818ebae3a5c08eb
Basix <basix@basix.tech>
2024-06-23 08:54:11 +0900

    Enable mini.files

--- dot_config/nvim/lua/config/mini.lua
+++ dot_config/nvim/lua/config/mini.lua
@@ -1,37 +1,39 @@
 return {
-	'echasnovski/mini.nvim',
-	version = '*',
-	config = function()
-		require('mini.ai').setup()
+  'echasnovski/mini.nvim',
+  version = '*',
+  config = function()
+    require('mini.ai').setup()
 
-		require('ts_context_commentstring').setup {
-			enable_autocmd = false,
-		}
+    require('ts_context_commentstring').setup {
+      enable_autocmd = false,
+    }
 
-		require('mini.comment').setup {
-			options = {
-				custom_commentstring = function()
-					return require('ts_context_commentstring').calculate_commentstring() or vim.bo.commentstring
-				end,
-			},
-		}
+    require('mini.comment').setup {
+      options = {
+        custom_commentstring = function()
+          return require('ts_context_commentstring').calculate_commentstring() or vim.bo.commentstring
+        end,
+      },
+    }
 
-		require('mini.animate').setup {
-			cursor = { enable = false },
-			resize = { enable = false },
-			open = { enable = false },
-			close = { enable = false },
-		}
+    require('mini.animate').setup {
+      cursor = { enable = false },
+      resize = { enable = false },
+      open = { enable = false },
+      close = { enable = false },
+    }
 
-		require('mini.splitjoin').setup {
-			mappings = {
-				toggle = 'gs',
-			},
-		}
+    require('mini.splitjoin').setup {
+      mappings = {
+        toggle = 'gs',
+      },
+    }
 
-		require('mini.operators').setup()
-	end,
-	dependencies = {
-		'JoosepAlviste/nvim-ts-context-commentstring'
-	}
+    require('mini.operators').setup()
+    require('mini.files').setup()
+		vim.keymap.set('n', '<leader>-', MiniFiles.open)
+  end,
+  dependencies = {
+    'JoosepAlviste/nvim-ts-context-commentstring',
+  },
 }
