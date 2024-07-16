f4d5d9986b8902eb9a2d1d055b5e24bc049d59f2
Basix <basix@basix.tech>
2024-07-16 16:57:00 +0900

    Add yarnrc schema for yamlls

--- dot_config/nvim/lua/custom/lsp.lua
+++ dot_config/nvim/lua/custom/lsp.lua
@@ -89,26 +89,25 @@ local servers = {
 
   yamlls = {
     schemas = {
-      kubernetes = "*.yaml",
-      ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
-      ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-      ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
-      ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
-      ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
-      ["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
-      ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
-      ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
-      ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
-      ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
-      ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] =
-      "*docker-compose*.{yml,yaml}",
-      ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] =
-      "*flow*.{yml,yaml}",
-      ["https://taskfile.dev/schema.json"] = "Taskfile.yml"
+      kubernetes = '*.yaml',
+      ['http://json.schemastore.org/github-workflow'] = '.github/workflows/*',
+      ['http://json.schemastore.org/github-action'] = '.github/action.{yml,yaml}',
+      ['http://json.schemastore.org/ansible-stable-2.9'] = 'roles/tasks/*.{yml,yaml}',
+      ['http://json.schemastore.org/prettierrc'] = '.prettierrc.{yml,yaml}',
+      ['http://json.schemastore.org/kustomization'] = 'kustomization.{yml,yaml}',
+      ['http://json.schemastore.org/ansible-playbook'] = '*play*.{yml,yaml}',
+      ['http://json.schemastore.org/chart'] = 'Chart.{yml,yaml}',
+      ['https://json.schemastore.org/dependabot-v2'] = '.github/dependabot.{yml,yaml}',
+      ['https://json.schemastore.org/gitlab-ci'] = '*gitlab-ci*.{yml,yaml}',
+      ['https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json'] = '*api*.{yml,yaml}',
+      ['https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json'] = '*docker-compose*.{yml,yaml}',
+      ['https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json'] = '*flow*.{yml,yaml}',
+      ['https://taskfile.dev/schema.json'] = 'Taskfile.yml',
+      ['https://yarnpkg.com/configuration/yarnrc.json'] = '.yarnrc.yml',
     }, -- https://www.reddit.com/r/neovim/comments/ze9gbe/comment/iz59clw/
   },
 
-  terraformls = {}
+  terraformls = {},
 }
 
 -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
@@ -163,6 +162,6 @@ null_ls.setup {
   },
 }
 
-require("typescript-tools").setup {
+require('typescript-tools').setup {
   on_attach = on_attach,
 }
