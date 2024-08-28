e9054af7ce1916ba235197f700ef7762629f3b85
Basix <basix@basix.tech>
2024-08-28 12:42:44 +0900

    Add keymap for Neogit

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -246,7 +246,7 @@ end
 vim.keymap.set('n', '<leader>s/', telescope_live_grep_open_files, { desc = '[S]earch [/] in Open Files' })
 vim.keymap.set('n', '<leader>ss', require('telescope.builtin').builtin, { desc = '[S]earch [S]elect Telescope' })
 vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-vim.keymap.set('n', '<leader>gsw', require('telescope.builtin').git_branches, { desc = '[G]it [Sw]itch' })
+vim.keymap.set('n', '<leader>gi', ':Neogit<CR>', { desc = '[G]it [I]nterface' })
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
