49a3e8600f01815b846ace991716933dc07b3354
Basix <basix@basix.tech>
2024-11-27 07:56:42 +0900

    Enable indent-line and neotree
    autoformat is now handled by conform thus redundant

---
+++ dot_config/nvim/lua/kickstart/plugins/neo-tree.lua
@@ -0,0 +1,24 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'MunifTanjim/nui.nvim',
+  },
+  cmd = 'Neotree',
+  keys = {
+    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+  },
+  opts = {
+    filesystem = {
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
+        },
+      },
+    },
+  },
+}
