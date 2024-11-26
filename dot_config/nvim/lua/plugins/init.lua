6b2d3a2cae139d9291ca8b283473dd1ebce776a8
Basix <basix@basix.tech>
2024-11-26 20:22:11 +0900

    Add nvim-spider

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -217,4 +217,30 @@ return {
     ft = { 'markdown', 'codecompanion' }, -- see ai.lua
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
   },
+  {
+    'chrisgrieser/nvim-spider',
+    keys = {
+      {
+        'e',
+        function()
+          require('spider').motion 'e'
+        end,
+        mode = { 'n', 'o', 'x' },
+      },
+      {
+        'w',
+        function()
+          require('spider').motion 'w'
+        end,
+        mode = { 'n', 'o', 'x' },
+      },
+      {
+        'b',
+        function()
+          require('spider').motion 'b'
+        end,
+        mode = { 'n', 'o', 'x' },
+      },
+    },
+  },
 }
