df208ac6ee21667b9ae638b14668063df4103256
Basix <basix@basix.tech>
2024-11-26 20:22:44 +0900

    Remove redundant devicons
    Icons should be served via mini.icons

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -204,9 +204,6 @@ require('lazy').setup({
         end,
       },
       { 'nvim-telescope/telescope-ui-select.nvim' },
-
-      -- Useful for getting pretty icons, but requires a Nerd Font.
-      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
     },
     config = function()
       -- Telescope is a fuzzy finder that comes with a lot of different things that
