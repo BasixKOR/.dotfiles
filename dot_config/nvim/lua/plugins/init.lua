904828012d154480d881d6409b7ff5924f77fbe7
Basix <basix@basix.tech>
2024-11-27 21:00:44 +0900

    Improve chezmoi experience on Neovim

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -116,8 +116,21 @@ return {
   {
     'xvzc/chezmoi.nvim',
     event = 'VeryLazy',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim' },
     opts = {},
+    config = function(_, opts)
+      vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+        pattern = { os.getenv 'HOME' .. '/.local/share/chezmoi/*' },
+        callback = function(ev)
+          local bufnr = ev.buf
+          vim.schedule(function()
+            require('chezmoi.commands.__edit').watch(bufnr)
+          end)
+        end,
+      })
+      require('chezmoi').setup(opts)
+      require('telescope').load_extension 'chezmoi'
+    end,
   },
   {
     'nvim-neorg/neorg',
