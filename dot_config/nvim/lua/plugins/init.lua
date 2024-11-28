5b3f4d968502afdaebb9f594512954a3fe123a49
Basix <basix@basix.tech>
2024-11-28 17:02:59 +0900

    Enable persistence.nvim keymappings

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -212,10 +212,20 @@ return {
   {
     'folke/persistence.nvim',
     event = 'BufReadPre', -- this will only start session saving when an actual file was opened
-    opts = {
-      -- add any custom options here
-    },
-    keys = {},
+    config = function()
+      vim.keymap.set('n', '<leader>qs', function()
+        require('persistence').load()
+      end)
+      vim.keymap.set('n', '<leader>qS', function()
+        require('persistence').select()
+      end)
+      vim.keymap.set('n', '<leader>ql', function()
+        require('persistence').load { last = true }
+      end)
+      vim.keymap.set('n', '<leader>qd', function()
+        require('persistence').stop()
+      end)
+    end,
   },
   {
     'MeanderingProgrammer/render-markdown.nvim',
