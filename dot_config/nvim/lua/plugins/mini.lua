db22e8954d7ce113639dcd394250c0c07302d601
Basix <basix@basix.tech>
2025-01-27 04:43:29 +0900

    Enable tpope-style surround

--- dot_config/nvim/lua/plugins/mini.lua
+++ dot_config/nvim/lua/plugins/mini.lua
@@ -32,11 +32,26 @@ return {
     }
 
     -- Add/delete/replace surroundings (brackets, quotes, etc.)
-    --
-    -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-    -- - sd'   - [S]urround [D]elete [']quotes
-    -- - sr)'  - [S]urround [R]eplace [)] [']
-    require('mini.surround').setup()
+    -- Configuration inspired by: https://github.com/echasnovski/mini.nvim/discussions/80#discussioncomment-2719781
+    -- Works similar to tpope/vim-surround
+    require('mini.surround').setup {
+      custom_surroundings = {
+        ['('] = { output = { left = '( ', right = ' )' } },
+        ['['] = { output = { left = '[ ', right = ' ]' } },
+        ['{'] = { output = { left = '{ ', right = ' }' } },
+        ['<'] = { output = { left = '< ', right = ' >' } },
+      },
+      mappings = {
+        add = 'ys',
+        delete = 'ds',
+        find = '',
+        find_left = '',
+        highlight = '',
+        replace = 'cs',
+        update_n_lines = '',
+      },
+      search_method = 'cover_or_next',
+    }
 
     -- Simple and easy statusline.
     --  You could remove this setup call if you don't like it,
