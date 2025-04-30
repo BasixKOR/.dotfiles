d6be72748d06a1c81cf675cd1230e5e0a9ab7ed0
Basix <basix@basix.tech>
2025-04-30 11:25:52 +0900

    Remove nvim-spider
    Causes more friction due to incompatiblities.

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -212,36 +212,6 @@ return {
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
   },
   {
-    'chrisgrieser/nvim-spider',
-    keys = {
-      {
-        'e',
-        function()
-          require('spider').motion 'e'
-        end,
-        mode = { 'n', 'o', 'x' },
-      },
-      {
-        'w',
-        function()
-          require('spider').motion 'w'
-        end,
-        mode = { 'n', 'o', 'x' },
-      },
-      {
-        'b',
-        function()
-          require('spider').motion 'b'
-        end,
-        mode = { 'n', 'o', 'x' },
-      },
-    },
-    opts = {
-      skipInsignificantPunctuation = false,
-      subwordMovement = true,
-    },
-  },
-  {
     'pwntester/octo.nvim',
     cmd = 'Octo',
     dependencies = {
