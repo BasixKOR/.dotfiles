89f930dae0acf30841ab48db88e01245d37cf295
Basix <basix@basix.tech>
2025-05-17 07:28:57 +0900

    Fix tsc to use Neovim API

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -86,16 +86,12 @@ return {
     event = 'VeryLazy',
     dependencies = { 'rcarriga/nvim-notify' },
     config = function()
-      local lspconfig = require 'lspconfig'
-      local util = lspconfig.util
-
-      local root_path = util.find_git_ancestor(vim.fn.getcwd())
-      local is_yarn_pnp = util.path.is_file(util.path.join(root_path, '.pnp.cjs'))
-
-      local tsc_path = is_yarn_pnp and util.path.join(root_path, '.yarn/sdks/typescript/bin/tsc') or nil
+      local pnp_path = vim.fs.find('.pnp.cjs', { path = vim.fn.getcwd(), upward = true })[1]
+      local is_yarn_pnp = (vim.uv.fs_stat(pnp_path) or {}).type == 'file'
+      local tsc_path = is_yarn_pnp and table.concat { vim.fs.dirmame(pnp_path), '/.yarn/sdks/typescript/bin/tsc' }
 
       require('tsc').setup {
-        bin_path = tsc_path,
+        bin_path = tsc_path or require('tsc.utils').find_tsc_bin(),
       }
     end,
   },
