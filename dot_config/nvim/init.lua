ebdd8dab5b169839ae1cc4451518f7fd6856bee0
Basix <basix@basix.tech>
2026-06-16 14:04:25 +0900

    neovim: specify kickstart.nvim copyright declarations

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,3 +1,7 @@
+-- SPDX-FileCopyrightText: 2024 TJ DeVries
+--
+-- SPDX-License-Identifier: MIT
+
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
